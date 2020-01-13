dd69eae361184fd24e3f95c664eab048d6f0d326
Victor Rajewski <askvictor@gmail.com>
2020-01-13 13:46:23 +1100

    clean up and finalise setup process

--- README.md
+++ README.md
@@ -4,16 +4,11 @@ Implements better CEC controls for Chromecasts. Currently has two functions:
 * sends CEC volume commands when Chromecast volume is changed (useful for surround-sound media which Chromecast doesn't let you control volume for)
 
 Tested on Raspberry Pi running Raspbian. 
-## Installation (without VirtualEnv) - TODO - test these instructions!
+## Installation (without VirtualEnv)
 1. Ensure Python 3.7 or later is installed (this is the default in Raspbian Buster)
 2. Clone this repository, or [download the zip](https://github.com/askvictor/ChromecastControls/archive/master.zip) and extract the files, then go into the directory thus created.
-3. Install requirements: `sudo pip3 install -r requirements.txt`
 3. Install CEC in the OS: `sudo apt-get install libcec-dev libcec4`
-4. Run `echo scan | cec-client -s -d 1` to show what devices are on the CEC bus. Find the device(s) you wish to power-down with this program, and remember their device numbers
-5. Edit chromecast_controls.py to change the config variables at the top of the file - set the number(s) from the previous step in CEC_DEV_ADDRS and the name of the Chromecast you wish to monitor as CHROMECAST_NAME. Optionally change the timeout value as well (this is how long it will wait between the Chromecast going idle until it switches off the device(s).
-6. Test that it works as planned (it's useful to change the timeout value to something small like 10 for testing). Run `python3 chromecast_controls.py` and connect and disconnect apps to the Chromecast to see if it works as desired. Ctrl-C to get out.
-7. Copy the main file to /usr/local/bin: `sudo cp chromecast_controls.py /usr/local/bin`. If you want to keep it somewhere else, that's fine but update the service file with the new location
-7. Choose a user to run as (pi is a good idea and will be used for the rest of this example, or you could create a dedicated user for this). Copy the systemd service file to /etc/systemd/system to allow automatic starting at boot time, replacing USERNAME with the user you want to run as: `sudo cp chromecast_controls@USERNAME.service /etc/systemd/system/chromecast_auto_standby@pi.service` 
-8. Start the system service `sudo service cc_standby@pi start`
+4. Install requirements: `sudo pip3 install -r requirements.txt`
+5. Run `sudo python3 chromecast_controls.py --setup` for interactive setup and install
 
 ## Installation (VirtualEnv) - TODO - write instructions
--- chromecast_controls.py
+++ chromecast_controls.py
@@ -5,18 +5,30 @@ import threading
 import cec
 import time
 import argparse
+import sys
+import os
+import shutil
+import subprocess
 
-parser = argparse.ArgumentParser(description='Auto-switches off TV/Hifi equipment (via CEC) if a particular Chromecast is inactive/idle.')
+parser = argparse.ArgumentParser(description='Checks if a particular Chromecast is idle, and switches off specified TV/Hifi equipment after a timeout. Also allows controlling of volume by CEC for surround-sound media.')
 parser.add_argument('--setup', action='store_true', help='Initial Setup')
 parser.add_argument('--chromecast', default='Living Room TV', help='Name of Chromecast to monitor (default: "Living Room TV"')
 parser.add_argument('--cec', type=int, default=5, help='Number of CEC device to monitor (default: 5)')
 parser.add_argument('--volume', dest='volume', action='store_true', help='Send CEC Volume commands when Chromecast changes volume')
 parser.set_defaults(volume=False)
+parser.add_argument('--standby', dest='standby', action='store_true', help='Put Hifi into standby when Chromecast is idle')
+parser.set_defaults(standby=True)
 parser.add_argument('--timeout', type=int, default=300, help='Time (seconds) between Chromecast going idle and device power down (default: 300 (=5min))')
 
 args = parser.parse_args()
 
+
 if args.setup:
+    if os.geteuid() == 0:  # we have root/sudo
+        pass
+    else:
+        print(f"To set up, please run as root:\n sudo ./{sys.argv[0].split('/')[-1]} --setup")
+
     print("Getting CEC Devices")
     cec.init()
     cec_devs = []
@@ -45,8 +57,38 @@ if args.setup:
             pass
         chromecast = input("Type the number of a listed device: ")
     cc = chromecasts[int(chromecast)].device.friendly_name
-    print(f'Options: --cec {cec_devs[0]} --chromecast "{cc}"')
+
+    vol = "z"
+    while vol not in "yn":
+        vol = input("Would you like to control hifi volume via Chromecast/phone controls? (y/n) ").lower()
+
+    sb = "z"
+    while sb not in "yn":
+        sb = input("Would you like to put hifi equipment into standby when Chromecast is idle? (y/n) ").lower()
+
+    options = f'--cec {cec_devs[0]} --chromecast "{cc}"{" --volume" if vol == "y" else ""}{" --standby" if sb == "y" else ""}'
+    if os.geteuid() == 0:  # we have root/sudo
+        print("copying myself into /usr/local/bin")
+        shutil.copy2(sys.argv[0], "/usr/local/bin")
+        print("creating systemd service")
+        template = open("chromecast_controls@USERNAME.service")
+        service_name = f'chromecast_controls@{os.getenv("SUDO_USER")}.service'
+        servicefile = open( f'/etc/systemd/system/{service_name}',"w")
+        for line in template:
+            if "ExecStart" in line:
+                line = line.rstrip() + " " + options
+            servicefile.write(line)
+        servicefile.close()
+        print("installing into systemd")
+        subprocess.run(["systemctl", "daemon-reload"])
+        print("starting Chromecast Control service")
+        subprocess.run(["systemctl", "start", service_name])
+    else:
+        print(f'Options for manual setup: {options}')
+
     exit()
+else:
+    print(f"To set up, please run:\n sudo ./{sys.argv[0].split('/')[-1]} --setup")
 
 CHROMECAST_NAME = args.chromecast
 TIMEOUT = args.timeout
@@ -54,8 +96,7 @@ CEC_DEV_ADDRS = [args.cec]
 
 # send CEC volume changes when Chromecast's volume has changed
 # this fixes the annoyance that CC won't change volume for media
-# with surround sound. Doesn't work once CC's internal volume counter
-# has reached its maximum
+# with surround sound.
 MIRROR_VOLUME = args.volume
 
 
